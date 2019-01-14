3786def49a4a10be311728ffc002851e68958440
2861892454@qq.com <2861892454@qq.com>
2019-01-14 14:21:31 +0800

    home change

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -6,28 +6,12 @@
 </template>
 <script>
 import HomeHeader from './components/Header'
-<<<<<<< HEAD
-<<<<<<< HEAD
 import HomeSwiper from './components/Swiper'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper
-=======
-export default {
-  name: 'Home',
-  components: {
-    HomeHeader
->>>>>>> a469e689cd9ca077d39acfb368df128867cb427a
-=======
-import HomeSwiper from './components/Swiper'
-export default {
-  name: 'Home',
-  components: {
-    HomeHeader,
-    HomeSwiper
->>>>>>> origin/index-swiper
   }
 }
 </script>

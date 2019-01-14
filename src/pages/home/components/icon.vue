820d3f10bf1eb0233e2b956cdd515510f1125486
2861892454@qq.com <2861892454@qq.com>
2019-01-14 17:12:47 +0800

    add icons

---
+++ src/pages/home/components/icon.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="icons">
+    <swiper class="swiper-content">
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-comtent" :src="item.imgURl" />
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      iconLish: [{
+        id: '001',
+        imgURl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '旅游景点'
+      },
+      {
+        id: '002',
+        imgURl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '旅游景点'
+      },
+      {
+        id: '003',
+        imgURl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '旅游景点'
+      },
+      {
+        id: '004',
+        imgURl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '旅游景点'
+      },
+      {
+        id: '005',
+        imgURl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '旅游景点'
+      },
+      {
+        id: '006',
+        imgURl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '旅游景点'
+      },
+      {
+        id: '007',
+        imgURl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '旅游景点'
+      },
+      {
+        id: '008',
+        imgURl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '旅游景点'
+      },
+      {
+        id: '009',
+        imgURl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '旅游景点'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconLish.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+<style lang='stylus' scoped>
+  @import '~styles/varibles.styl'
+  @import '~styles/mixins.styl'
+  .icons >>> .swiper-content
+    height 0
+    padding-bottom 50%
+  .icon
+    position relative
+    float left
+    overflow hidden
+    width 25%
+    padding-bottom 25%
+    .icon-img
+      position absolute
+      top 0
+      left 0
+      right 0
+      bottom .44rem
+      box-sizing: border-box
+      padding: .1rem
+      .icon-img-comtent
+        display block
+        margin 0 auto
+        height 100%
+    .icon-desc
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      height: .44rem
+      line-height: .44rem
+      text-align center
+      color $darkTextColor
+      ellipsis()
+</style>
